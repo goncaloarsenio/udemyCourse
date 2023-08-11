f64e389b21e575a1cccc06e703247e3fa6816d2a
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-08-11 12:45:25 +0100

    first commit- HTML

--- server.js
+++ server.js
@@ -14,20 +14,44 @@ mongoose
     app.emit("pronto");
   })
   .catch((e) => console.log(e));
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 const routes = require("./routes");
 const path = require("path");
-const { middlewareGlobal } = require("./src/middlewares/middleware");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 
+const sessionOptions = session({
+  secret: "akasdft2345345",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 + 7,
+    httpOnly: true,
+  },
+});
+
+app.use(sessionOptions);
+app.use(flash());
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
-//Nossos próprios middlewares
+app.use(csrf());
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on("pronto", () => {
